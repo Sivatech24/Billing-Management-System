5bbd0c226d5a55e8d19cf64fa3b4cfa142636cd7
SivaTech24 <sivatech24@gmail.com>
2026-02-19 17:53:25 +0530

    Update of the MainWindow.xaml code for the fixing of the view pages

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-// using Billing_Management_System.Views;
+using Billing_Management_System.Views;
 
 namespace Billing_Management_System
 {
