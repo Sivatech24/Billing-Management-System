ab26ee71dc16db514a9fe51064ae8e7ef1c1069a
SivaTech24 <sivatech24@gmail.com>
2026-02-19 16:50:06 +0530

    Add of the code mainwindow.xaml.cs code with errors need fix

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+// using Billing_Management_System.Views;
 
 namespace Billing_Management_System
 {
@@ -23,6 +24,49 @@ namespace Billing_Management_System
         public MainWindow()
         {
             InitializeComponent();
+            // Show dashboard on startup
+            // defer showing HomeView until component names are available
+            this.Loaded += (s, e) => { MainFrame.Content = new HomeView(); };
+        }
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            // Home view removed: clear the frame or navigate to a default existing view
+            MainFrame.Content = null;
+        }
+
+        private void Bill_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new BillingView();
+        }
+
+        private void Payments_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new PaymentsView();
+        }
+
+        private void AddData_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new AddDataView();
+        }
+
+        private void ViewData_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new ViewDataView();
+        }
+
+        private void Print_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new PrintView();
+        }
+
+        private void Options_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new OptionsView();
+        }
+
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = new HelpView();
         }
     }
 }
