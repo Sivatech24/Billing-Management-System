e3e809e6af972ac19e573450abfd0f81314f9f9b
SivaTech24 <sivatech24@gmail.com>
2026-02-19 18:18:13 +0530

    Update of the code of the billitem.cs

--- Views/BillItem.cs
+++ Views/BillItem.cs
@@ -4,9 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Billing_Management_System.Views
+namespace Billing_Management_System.Models
 {
-    internal class BillItem
+    //internal class BillItem
+    //{
+    //}
+    public class BillItem
     {
+        public long Id { get; set; }
+        public long BillId { get; set; }
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+        public int Qty { get; set; }
+        public decimal Cgst { get; set; }
+        public decimal Sgst { get; set; }
+        public decimal Total { get; set; }
     }
 }
