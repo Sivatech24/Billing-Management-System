c578237fc599ca337e153018e71a1aaefc6e874e
SivaTech24 <sivatech24@gmail.com>
2026-02-19 16:13:05 +0530

    Update of the code of the Securityhelper.cs file

--- SecurityHelper.cs
+++ SecurityHelper.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Security.Cryptography;
 using System.Text;
-using System.Threading.Tasks;
 
-namespace Billing_Management_System
+namespace BillingApp
 {
-    internal class SecurityHelper
+    public static class SecurityHelper
     {
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha = SHA256.Create())
+            {
+                byte[] bytes = sha.ComputeHash(Encoding.UTF8.GetBytes(password));
+                StringBuilder builder = new StringBuilder();
+
+                foreach (byte b in bytes)
+                    builder.Append(b.ToString("x2"));
+
+                return builder.ToString();
+            }
+        }
     }
 }
